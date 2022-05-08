dcd7e9e556c27c503f28c444472924d5f4352249
Reza Pasalar <rezapasalar@yahoo.com>
2022-05-08 18:45:14 +0430

    Website template components as well as Dark mode were designed.

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,7 @@
+export default () => {
+    return (
+        <footer className="text-center bg-gray-200 dark:bg-gray-800 p-8">
+            <span className="text-gray-500 dark:text-white">تمامی حقوق محفوظ است </span>
+        </footer>
+    )
+}
