a84d5f71061d7549782d2f5aab989f930ceddff2
Reza Pasalar <rezapasalar@yahoo.com>
2022-05-15 10:47:28 +0430

    Fixed a bug in the DarkModeLayout.js component.

--- src/components/layouts/DarkModeLayout.js
+++ src/components/layouts/DarkModeLayout.js
@@ -15,8 +15,8 @@ const DarkModeLayout = () => {
      */
     const changeMode = theme => {
         (theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches))
-            ? document.documentElement.classList.add('dark')
-            : document.documentElement.classList.remove('dark')
+            ? window.document.documentElement.classList.add('dark')
+            : window.document.documentElement.classList.remove('dark')
 
         setMode(theme)
         localStorage.theme = theme
